23014a457a55a4048601f64137e1a7bc539bb6c9
Tom Cardenas <eruditus.productions@gmail.com>
2017-11-30 21:40:18 -0500

    Initial Commit

---
+++ d·Browser/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  d·Browser
+//
+//  Created by System Administrator on 30/11/17.
+//  Copyright © 2017 Eruditus-Developers ©. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ d·Browser/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  d·Browser
+//
+//  Created by System Administrator on 30/11/17.
+//  Copyright © 2017 Eruditus-Developers ©. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
